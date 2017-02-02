e41cc9c96f6b66734e51dfd5fcf19e9389fea001
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-02-02 23:26:53 +0200

    Add scroll effect and tooltips, change colors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// window.addEventListener("hashchange", function() { scrollBy(0, -50) })
+
+var $root = $('html, body');
+
+$('a').click(function() {
+	$root.animate({
+		scrollTop: $(this).attr("href") == "#about" ? 0 : $($.attr(this, 'href')).offset().top
+	}, 500);
+
+    return false;
+});
