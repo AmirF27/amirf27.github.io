4a2dd4dfa4683b994146d01efdb228a8d904a5c3
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-02-04 01:28:51 +0200

    Add active tab switching functionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $("[data-toggle='tooltip']").tooltip()
 })
 
-// window.addEventListener("hashchange", function() { scrollBy(0, -50) })
+var $root = $("html, body");
 
-var $root = $('html, body');
-
-$('a').click(function() {
-	$root.animate({
-		scrollTop: $($.attr(this, 'href')).offset().top - ($(this).attr("href") == "#about" ? 80 : 60)
-	}, 500);
+$(".navbar-nav a").click(function() {
+  $root.animate({
+    scrollTop: $($.attr(this, "href")).offset().top - ($(this).attr("href") == "#about" ? 80 : 60)
+  }, 500);
 
     return false;
 });
+
+$(document).on("scroll", function() {
+  if(!$(".navbar-nav a[href='#about']").parent().hasClass("active") &&
+    $(this).scrollTop() >= $("#about").position().top - 80 &&
+    $(this).scrollTop() <= $("#projects").position().top - 180) {
+      changeActive("#about");
+  }
+  else if (!$(".navbar-nav a[href='#projects']").parent().hasClass("active") &&
+    $(this).scrollTop() >= $("#projects").position().top - 180 &&
+    $(this).scrollTop() <= $("#contact").position().top - 180) {
+      changeActive("#projects");
+  }
+  else if (!$(".navbar-nav a[href='#contact']").parent().hasClass("active") &&
+    $(this).scrollTop() >= $("#contact").position().top - 60) {
+      changeActive("#contact");
+  }
+});
+
+var changeActive = function(target) {
+  $(".active").removeClass("active");
+  $(".navbar-nav a[href='" + target + "']").parent().addClass("active");
+  console.log(target);
+}
