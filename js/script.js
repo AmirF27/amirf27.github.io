9a4283f95718e91014c9bae47e56410d49d10285
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-02-04 19:41:43 +0200

    Close navbar on item click

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
-$(function () {
-  $("[data-toggle='tooltip']").tooltip()
-})
+$(function() {
+  // to be used later for smooth scroll animation
+  var $root = $("html, body");
 
-var $root = $("html, body");
+  var calcOffset = function(anchor) {
+    var offset;
 
-$(".navbar-nav a").click(function() {
-  $root.animate({
-    scrollTop: calcOffset(this)
-  }, 700);
+    offset = $($.attr(anchor, "href")).offset().top - 60;
+
+    if ($(anchor).attr("href") == "#about") {
+      offset -= 20;
+    }
+    else if ($(window).width() < 768) {
+      offset += 10;
+    }
+
+    return offset;
+  }
+
+  var changeActive = function(target) {
+    $(".active").removeClass("active");
+    $(".navbar-nav a[href='" + target + "']").parent().addClass("active");
+  }
+
+  $("[data-toggle='tooltip']").tooltip();
+
+  $(".nav a").on("click", function(){
+    $(".navbar-collapse").collapse("hide");
+
+    $root.animate({
+      scrollTop: calcOffset(this)
+    }, 700);
 
     return false;
+  });
+
+  $(document).on("scroll", function() {
+    if(!$(".navbar-nav a[href='#about']").parent().hasClass("active") &&
+      $(this).scrollTop() >= $("#about").position().top - 80 &&
+      $(this).scrollTop() <= $("#projects").position().top - 180) {
+        changeActive("#about");
+    }
+    else if (!$(".navbar-nav a[href='#projects']").parent().hasClass("active") &&
+      $(this).scrollTop() >= $("#projects").position().top - 180 &&
+      $(this).scrollTop() <= $("#contact").position().top - 180) {
+        changeActive("#projects");
+    }
+    else if (!$(".navbar-nav a[href='#contact']").parent().hasClass("active") &&
+      $(this).scrollTop() >= $("#contact").position().top - 180) {
+        changeActive("#contact");
+    }
+  });
 });
-
-$(document).on("scroll", function() {
-  if(!$(".navbar-nav a[href='#about']").parent().hasClass("active") &&
-    $(this).scrollTop() >= $("#about").position().top - 80 &&
-    $(this).scrollTop() <= $("#projects").position().top - 180) {
-      changeActive("#about");
-  }
-  else if (!$(".navbar-nav a[href='#projects']").parent().hasClass("active") &&
-    $(this).scrollTop() >= $("#projects").position().top - 180 &&
-    $(this).scrollTop() <= $("#contact").position().top - 180) {
-      changeActive("#projects");
-  }
-  else if (!$(".navbar-nav a[href='#contact']").parent().hasClass("active") &&
-    $(this).scrollTop() >= $("#contact").position().top - 180) {
-      changeActive("#contact");
-  }
-});
-
-var calcOffset = function(anchor) {
-  var offset;
-
-  if ($(window).width() < 768) {
-    offset = $($.attr(anchor, "href")).offset().top - 50;
-  }
-  else {
-    offset = $($.attr(anchor, "href")).offset().top - ($(anchor).attr("href") == "#about" ? 80 : 60);
-  }
-
-  return offset;
-}
-
-var changeActive = function(target) {
-  $(".active").removeClass("active");
-  $(".navbar-nav a[href='" + target + "']").parent().addClass("active");
-}
